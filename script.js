ec3239b6f63ffee3d12d770ac9a7dcd03c61577c
Samuel Nelson <snelson54@cnm.edu>
2019-05-14 10:34:34 -0600

    Added findMyText if statement

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ function findMyText(needle, replacement) {
 	if (haystackText.length == 0) {
 		haystackText = document.getElementById("haystack").innerHTML;
 	}
-	var match
+	var match = new RegExp(needle, "ig");
+	var replaced = "";
+	if (replacement.length > 0) {
+		replaced = haystackText.replace(match, replacement);
+	}
+	else
 }
