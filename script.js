ebc7ee6e0f1952f66072d8f93ba641fe64cd4902
Samuel Nelson <snelson54@cnm.edu>
2019-05-14 10:30:15 -0600

    Started findMyText function

---
+++ script.js
@@ -0,0 +1,7 @@
+var haystackText = "";
+function findMyText(needle, replacement) {
+	if (haystackText.length == 0) {
+		haystackText = document.getElementById("haystack").innerHTML;
+	}
+	var match
+}
